1246507e90ccf655386a33f88572a3704eef0c3e
K31Sonawane <44919541+K31Sonawane@users.noreply.github.com>
2026-02-05 12:19:19 +0530

    Update graphApi.ts

--- src/api/graphApi.ts
+++ src/api/graphApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:3000/api"
+export const api = axios.create({
+  baseURL: "https://insightboard-backend.vercel.app/api", 
 });
 
 export const submitTranscript = (transcript: string) =>
